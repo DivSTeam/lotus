cfdffaab1699b669e6da354cffa733887023bcda
depchai1511 <natibatian@gmail.com>
2023-04-01 17:43:14 +0300

    fix(schema): add array of favorite products to schema

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'
+import crypto from 'crypto'
 
 const userSchema = new mongoose.Schema(
   {
@@ -6,11 +8,36 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false },
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+    refreshToken: {type: String,},
+    passwordChangedAt: {type: String},
+    passwordResetToken: {type: String},
+    passwordResetExpires: {type: String}
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+      next()
+  }
+  const salt = bcrypt.genSaltSync(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+userSchema.methods = {
+  isCorrectPassword: async function (password) {
+    return await bcrypt.compare(password, this.password)
+  },
+  createPasswordChangedToken: function () {
+    const resetToken = crypto.randomBytes(32).toString('hex')
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+    this.passwordResetExpires = Date.now() + 15 * 60 * 1000
+    return resetToken
+}
+}
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 export default User;
