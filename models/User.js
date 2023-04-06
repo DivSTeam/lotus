d0f01f9c071f6268fb40c85c96f91d274f238185
depchai1511 <natibatian@gmail.com>
2023-04-06 03:28:42 +0300

    No change!

--- models/User.js
+++ models/User.js
@@ -1,43 +1,34 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt'
 import crypto from 'crypto'
+import emailValidator from 'email-validator';
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { 
+      type: String, 
+      required: true, 
+      unique: true,
+    },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false },
     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
     refreshToken: {type: String,},
     passwordChangedAt: {type: String},
     passwordResetToken: {type: String},
-    passwordResetExpires: {type: String}
+    passwordResetExpires: {type: String},
+    verificationToken : {type : String}, 
+    isVerified: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     timestamps: true,
   }
 );
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-      next()
-  }
-  const salt = bcrypt.genSaltSync(10)
-  this.password = await bcrypt.hash(this.password, salt)
-})
-
-userSchema.methods = {
-  isCorrectPassword: async function (password) {
-    return await bcrypt.compare(password, this.password)
-  },
-  createPasswordChangedToken: function () {
-    const resetToken = crypto.randomBytes(32).toString('hex')
-    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex')
-    this.passwordResetExpires = Date.now() + 15 * 60 * 1000
-    return resetToken
-}
-}
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 export default User;
