735708b49db1cb90490797f8f74d8e86c8601f77
depchai1511 <natibatian@gmail.com>
2023-04-29 19:49:57 +0300

    fix(schema) : add some fields

--- models/Product.js
+++ models/Product.js
@@ -9,11 +9,14 @@ const productSchema = new mongoose.Schema(
     price: { type: Number, required: true },
     brand: { type: String, required: true },
     rating: { type: Number, required: true, default: 0 },
+    isFeatured: { type: Boolean, default: false },
     numReviews: { type: Number, required: true, default: 0 },
     countInStock: { type: Number, required: true, default: 0 },
     description: { type: String, required: true },
     isFeatured: { type: Boolean, default: false },
     banner: String,
+    createdAt: { type: Date, default: Date.now },
+    
   },
   {
     timestamps: true,
